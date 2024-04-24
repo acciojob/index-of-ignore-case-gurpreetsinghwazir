75fa1d9c60e260d1532c3b15ba21c814599e6c2c
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-04-24 23:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function indexOfIgnoreCase(s1, s2) {
 	let s1Low = s1.toLowerCase();
     let s2Low = s2.toLowerCase();	
     let text = s1Low.indexOf(s2Low);
-    console.log(text);
+    return(text);
 
 	
 }
@@ -10,4 +10,4 @@ function indexOfIgnoreCase(s1, s2) {
 // Please do not change the code below
  const s1 = prompt("Enter s1:");
  const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+	alert(indexOfIgnoreCase(s1, s2));
